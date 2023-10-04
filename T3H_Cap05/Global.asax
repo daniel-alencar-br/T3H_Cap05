b808d9c2b3e94d65817667e5d62e10726e13c5f8
daniel-alencar-br <danielalencar.br@gmail.com>
2023-10-03 21:08:48 -0300

    Adicionar arquivos de projeto.

---
+++ T3H_Cap05/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="T3H_Cap05.MvcApplication" Language="C#" %>
---
+++ T3H_Cap05/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(T3H_Cap05.Startup))]
+namespace T3H_Cap05
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
